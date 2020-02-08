66278575bde47b4382c4b4b6d667405a577fcf72
Raphaël Akladios <raphael.akladios@gmail.com>
2020-02-08 22:23:50 +0100

    rename_vftable_funcs.py created

--- Ghidra_scripts/rename_vftable_funcs.py
+++ Ghidra_scripts/rename_vftable_funcs.py
@@ -1,13 +1,13 @@
 # Renames functions of a vftable who still have their default name "FUN_0xsomething"
 #@category TheirGlamorousNoobness
 # The script expects the currentAddress to be equal to the beginning of the vftable
-# A label is added at the beginning of the vftable, functions with default names are renamed
+# The default "vftable" symbol name is renamed to improve readability
 # __thiscall and custom variable storage are set to all of the vftable's functions
 
-from ghidra.program.model.symbol import SourceType
-from ghidra.app.cmd.label import AddLabelCmd
+from ghidra.program.model.symbol import SourceType, SymbolType
 
 vftableAddress = currentAddress
+naming_prefix = "TGN_"
 
 code_manager = currentProgram.getCodeManager()
 func_manager = currentProgram.getFunctionManager()
@@ -15,12 +15,9 @@ func_manager = currentProgram.getFunctionManager()
 d = code_manager.getDataAt(vftableAddress)
 assert(d.isArray())
 
-vftable_title = d.getComment(3).replace('const ', '').split('::')
-assert(vftable_title[1]=='vftable')
-class_name = vftable_title[0]
-
-symbols = currentProgram.getSymbolTable().getGlobalSymbols(class_name)
-assert (any(symb.getSymbolType().toString() == "Class" for symb in symbols))
+assert(d.getPrimarySymbol().getName() == "vftable")
+assert(d.getPrimarySymbol().getParentNamespace().getSymbol().getSymbolType()==SymbolType.CLASS)
+class_name = d.getPrimarySymbol().getParentNamespace().getName()
 
 for i in range(d.getNumComponents()):
   comp = d.getComponent(i)
@@ -30,8 +27,7 @@ for i in range(d.getNumComponents()):
   assert(func!=None)
   func_name = func.getName()
   if func_name == 'FUN_'+func_addr.toString() and func.getSymbol().getSource() == SourceType.DEFAULT:
-    func.setName('TGN_'+class_name+'_vftable'+comp.getFieldName(), SourceType.USER_DEFINED)
+    func.setName(naming_prefix+class_name+'_vftable'+comp.getFieldName(), SourceType.USER_DEFINED)
   func.setCallingConvention('__thiscall')
   func.setCustomVariableStorage(True)
-AddLabelCmd(vftableAddress, 'TGN_'+class_name+"_vftable", SourceType.USER_DEFINED).applyTo(currentProgram)
-
+d.getPrimarySymbol().setName(naming_prefix+class_name+"_vftable", SourceType.USER_DEFINED)
